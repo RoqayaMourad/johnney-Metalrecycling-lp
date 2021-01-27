160eeb431b6024c54dfa481bb30afd7d53b07b5f
roqaya299 <elephantsong29@example.com>
2021-01-27 19:30:35 +0200

    check form!

--- contact.php
+++ contact.php
@@ -1,12 +1,21 @@
 
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Message: $message";
-$recipient = "leads@lp-quadsolutions.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$message=$_POST['message'];
+
+		$to='leads@lp-quadsolutions.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."subject :".$subject."\n"."Wrote the following :"."\n\n".$message;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
